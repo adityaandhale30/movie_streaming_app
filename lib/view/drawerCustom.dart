7a2413e371c6c4fd9d7fadb1c56f7209cba45bc7
AdiByte30 <adityaandhale30@gmail.com>
2024-09-24 15:22:10 +0530

    drawer built

--- lib/view/drawerCustom.dart
+++ lib/view/drawerCustom.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DrawerCustom extends StatelessWidget {
   const DrawerCustom({super.key});
@@ -6,27 +9,68 @@ class DrawerCustom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: Color.fromRGBO(10, 7, 30, 1),
-      width: 200,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-            height: 30,
-            width: 50,
-            color: Colors.blueAccent,
-          ),
-          Container(
-            height: 30,
-            width: 50,
-            color: Colors.blueAccent,
-          ),
-          Container(
-            height: 30,
-            width: 50,
-            color: Colors.blueAccent,
-          ),
-        ],
+      backgroundColor: const Color.fromRGBO(10, 7, 30, 1),
+      width: 220,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16, top: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(
+              onPressed: () {
+                Get.back();
+              },
+              icon: const Icon(
+                Icons.close_outlined,
+                size: 28,
+                color: Colors.white,
+              ),
+            ),
+            Column(
+              children: List.generate(6, (idx) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 24, top: 24),
+                  child: Text(
+                    "Series",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              }),
+            ),
+            const Divider(
+              color: Color.fromRGBO(255, 255, 255, 0.5),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, bottom: 8),
+              child: Row(
+                children: [
+                  Text(
+                    "Genders",
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(),
+                  const Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: Color.fromRGBO(255, 255, 255, 0.726),
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              color: Color.fromRGBO(255, 255, 255, 0.5),
+            ),
+            const Gap(30),
+            Image.asset("assets/images/movieLogo.png")
+          ],
+        ),
       ),
     );
   }
